40d3b484a7345e437dc9d55d24e8602c4f164823
Ankit Sah <ankitkumarsah3727@gmail.com>
2021-04-03 16:55:21 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+app.get("/",function(req,res){
+    res.send("prerna");
+});
+app.get("/prerna",function(req,res){
+    res.send("ghjk");
+});
+
+
+app.listen(80);
